3ed926967fe34ab8ffb9a7d2ddfac68b7e54f12d
shinshin86 <beagles1986@gmail.com>
2017-01-09 16:26:46 +0900

    Add to setup gnu-sed

--- macOS_Sierra/setup_scripts/nvm_setup.sh
+++ macOS_Sierra/setup_scripts/nvm_setup.sh
@@ -5,7 +5,7 @@
 #====================================================================
 
 echo "nvm setting : start..."
-
+brew install gnu-sed
 git clone https://github.com/creationix/nvm.git ~/.nvm && cd ~/.nvm && git checkout `git describe --abbrev=0 --tags`
 
 echo 'source ~/.nvm/nvm.sh' >> ~/.zshrc
