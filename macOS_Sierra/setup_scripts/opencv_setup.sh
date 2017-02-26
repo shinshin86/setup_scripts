0d6b3d103a5c13d3962bc69cf6fa344566b6592f
shinshin86 <beagles1986@gmail.com>
2017-02-26 09:13:19 +0900

    Add opencv_setup, and modify exe auth.

---
+++ macOS_Sierra/setup_scripts/opencv_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#====================================================================
+# OpenCV setup
+#====================================================================
+
+brew update
+brew install -v cmake
+brew tap homebrew/science
+brew install opencv
+
+echo "OpenCV setup successful!!"
