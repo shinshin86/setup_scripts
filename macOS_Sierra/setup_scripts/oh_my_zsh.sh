6da48bf970a491b3bf2a201af89dc2c707c77162
shinshin86 <beagles1986@gmail.com>
2017-12-23 20:22:45 +0900

    Modify setup of oh my zsh.

--- macOS_Sierra/setup_scripts/oh_my_zsh.sh
+++ macOS_Sierra/setup_scripts/oh_my_zsh.sh
@@ -6,9 +6,9 @@
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "export PATH=/usr/local/bin:$PATH" >> ~/.zhsrc
-echo 'ZSH_THEME="mh"' >> ~/.zhsrc
-echo "export PATH=/usr/local/Homebrew/bin:$PATH" >> ~/.zhsrc
-echo "fpath=(/usr/local/share/zsh-completions $fpath)" >> ~/.zhsrc
-echo "plugins=(git ruby osx bundler brew rails emoji-clock)" >> ~/.zhsrc
-echo 'PROMPT="%K{17}%~ : $%K"' >> ~/.zhsrc
+echo "export PATH=/usr/local/bin:$PATH" >> ~/.zshrc
+echo 'ZSH_THEME="mh"' >> ~/.zshrc
+echo "export PATH=/usr/local/Homebrew/bin:$PATH" >> ~/.zshrc
+echo "fpath=(/usr/local/share/zsh-completions $fpath)" >> ~/.zshrc
+echo "plugins=(git ruby osx bundler brew rails emoji-clock)" >> ~/.zshrc
+echo 'PROMPT="%K{17}%~ : $%K"' >> ~/.zshrc
