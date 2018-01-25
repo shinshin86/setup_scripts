2fa8faa898a0c4443a4b225488f55f74b303b94f
shinshin86 <beagles1986@gmail.com>
2018-01-25 22:20:13 +0900

    bugfix

--- macOS_Sierra/setup_scripts/oh_my_zsh.sh
+++ macOS_Sierra/setup_scripts/oh_my_zsh.sh
@@ -7,8 +7,8 @@
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 echo "export PATH=/usr/local/bin:$PATH" >> ~/.zshrc
-echo 'ZSH_THEME="mh"' >> ~/.zshrc
 echo "export PATH=/usr/local/Homebrew/bin:$PATH" >> ~/.zshrc
+echo 'ZSH_THEME="mh"' >> ~/.zshrc
 echo "fpath=(/usr/local/share/zsh-completions $fpath)" >> ~/.zshrc
 echo "plugins=(git ruby osx bundler brew rails emoji-clock)" >> ~/.zshrc
 echo 'PROMPT="%K{17}%~ : $%K"' >> ~/.zshrc
