647f814043131282be2ce3ef193b356375480fd2
shinshin86 <beagles1986@gmail.com>
2018-02-11 15:45:37 +0900

    Update setup of golang.

--- macOS_Sierra/setup_scripts/golang_setup.sh
+++ macOS_Sierra/setup_scripts/golang_setup.sh
@@ -9,6 +9,10 @@ brew install qt5 pkg-config
 brew link --force qt5
 
 mkdir ~/gocode
-echo "export GOPATH=\$HOME/gocode" >> ~/.zshrc
+echo "export GOPATH=\$HOME/go" >> ~/.zshrc
 echo "# setup golang" >> ~/.zshrc
 echo "export PATH=/usr/local/Cellar/go/*/bin:\$PATH" >> ~/.zshrc
+echo "export PATH=$PATH:$GOPATH/bin" >> ~/.zshrc
+echo "# gore alias" >> ~/.zshrc
+echo "alias gore-i='gore -autoimport'" >> ~/.zshrc
+
