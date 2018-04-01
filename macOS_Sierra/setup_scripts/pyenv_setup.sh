8ca8b3c1dced8615c1d8899a71cb338737781ad0
shinshin86 <beagles1986@gmail.com>
2018-04-01 17:35:46 +0900

    Update

--- macOS_Sierra/setup_scripts/pyenv_setup.sh
+++ macOS_Sierra/setup_scripts/pyenv_setup.sh
@@ -15,6 +15,7 @@ export PYENV_ROOT="${HOME}/.pyenv"\n
 if [ -d "${PYENV_ROOT}" ]; then\n
 export PATH=${PYENV_ROOT}/bin:$PATH\n
 eval "$(pyenv init -)"\n
+eval "$(pyenv virtualenv-init -)"\n
 fi\n
 ' >> ~/.zshrc
 
@@ -23,3 +24,6 @@ echo "pyenv install -l "
 echo "pyenv install {Python Version}"
 echo "pyenv global {Python2 Version} {Python3 Version}"
 echo "Example => pyenv global 2.7.13 3.5.2 "
+echo ""
+echo "TODO ===> validation this... : "
+echo "And install pyenv-virutalenv ==> brew install pyenv-virtualenv"
