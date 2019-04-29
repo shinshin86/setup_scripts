08e64eaf32e30d7d3040c96eafca0a9eca5e597d
shinshin86 <beagles1986@gmail.com>
2019-04-29 21:25:19 +0900

    Add setting for .zshrc

--- macOS/setup_scripts/fzf_setup.sh
+++ macOS/setup_scripts/fzf_setup.sh
@@ -2,3 +2,9 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 source ~/.zshrc
+
+# setup .zshrc
+echo '[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh' >> ~/.zshrc
+echo 'export FZF_DEFAULT_COMMAND='"'"'rg --files --hidden --glob "!.git"'"'" >> ~/.zshrc
+echo 'export FZF_DEFAULT_OPTS='"'"'--height 40% --reverse --border'"'" >> ~/.zshrc
+
